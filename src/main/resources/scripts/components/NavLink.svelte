6300b35cf70b2eda209ecc3e18d9b2bc87c0189b
Eugene Dementjev <daemos@x-cart.com>
2020-08-25 13:38:26 +0400

    Remove about page and redo navbar

--- src/main/resources/scripts/components/NavLink.svelte
+++ src/main/resources/scripts/components/NavLink.svelte
@@ -2,9 +2,14 @@
   import { Link } from "svelte-routing";
 
   export let to = "";
+  export let disabled = false
 
   function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === "/" ? isCurrent : isPartiallyCurrent || isCurrent;
+
+    if (disabled) {
+      return { class: "nav-link disabled" }
+    }
 
     // The object returned here is spread on the anchor element's attributes
     if (isActive) {
--- src/main/resources/scripts/pages/About.svelte
+++
@@ -1,3 +0,0 @@
-<section class="page">
-    <p>Nothing to see here yet</p>
-</section>
