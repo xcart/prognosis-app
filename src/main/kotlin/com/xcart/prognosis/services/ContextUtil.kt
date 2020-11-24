5ac8dd119360ad7410e9fb2a90bf65d68779eadc
Eugene Dementjev <daemos@x-cart.com>
2020-11-24 17:21:23 +0400

    Mock XB Api in tests

--- src/main/kotlin/com/xcart/prognosis/services/ContextUtil.kt
+++ src/main/kotlin/com/xcart/prognosis/services/ContextUtil.kt
@@ -5,9 +5,6 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.stereotype.Service
 
-/**
- * @author Naresh Joshi
- */
 @Service
 class ContextUtil : ApplicationContextAware {
     @Throws(BeansException::class)
